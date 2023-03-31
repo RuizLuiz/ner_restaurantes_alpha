850f21b118c9a33da6cdbfa17ee11eb21488cccd
Luiz Ruiz <luizruiz04@gmail.com>
2023-03-31 07:11:51 -0400

    First commit
    First doc uploads

---
+++ classes_balanceadas/trocas_bebida.py
@@ -0,0 +1,38 @@
+troca linha 31 fold 1
+"Ótimo prato, chopp gelado, vinho branco muito bom...excelente atendimento! O Garçom Edvaldo nos atendeu muito bem!!!","{'entities': [(6, 11, 'COMIDA'), (13, 18, 'BEBIDA'), (27, 39, 'BEBIDA'), (62, 73, 'SERVIÇO'), (77, 91, 'SERVIÇO')]}"
+pela linha 25 fold 4
+"Prato generoso para 2 pessoas! Muito saboroso, porém possui coentro e não especificam no cardápio! Como não gosto, tive que separar.","{'entities': [(0, 5, 'COMIDA'), (60, 67, 'COMIDA'), (89, 97, 'COMIDA')]}"
+troca linha 84 fold 1
+"O Coco Bambu de Uberlândia segue o padrão da rede que, em geral, é bom. Porém, a unidade local sofre de um sério problema com o serviço. O tempo para trazerem uma simples água é inacreditável. Uma bebida mais elaborada, pior ainda. Já os pratos......Mais","{'entities': [(89, 94, 'GERAL'), (238, 244, 'COMIDA'), (171, 175, 'BEBIDA'), (197, 203, 'BEBIDA'), (128, 135, 'SERVIÇO'), (2, 26, 'LOCALIZAÇÃO')]}"
+pela linha 47 fold 4
+"Fomos atendidos pelo Domingos Silva, muito atencioso, comida muito boa e ambiente agradável. RecomendoMais","{'entities': [(73, 81, 'AMBIENTE'), (54, 60, 'COMIDA'), (21, 35, 'SERVIÇO')]}"
+troca linha 96 fold 1
+"Tradicionalmente o melhor camarão da cidade, com ambiente agradável com vista para o mar; boa disponibilidade de vinhos e cervejas especiais. Bom para almoço, jantar e confraternizações.","{'entities': [(49, 57, 'AMBIENTE'), (72, 77, 'AMBIENTE'), (26, 33, 'COMIDA'), (151, 157, 'COMIDA'), (159, 165, 'COMIDA'), (113, 119, 'BEBIDA'), (122, 140, 'BEBIDA')]}"
+pela linha 2 fold 3
+"O Eduardo foi bastante atencioso, ótimo atendimento! A comida é deliciosa e caprichada, vale a visita.","{'entities': [(55, 61, 'COMIDA'), (2, 9, 'SERVIÇO'), (40, 51, 'SERVIÇO')]}"
+troca linha 54 fold 5
+"Excelente atendimento. Todos os profissionais, com quem tivemos contato,nos atenderam muito bem. Preocuparam-se com cada detalhe. Até o suco que meu filho derramou na roupa eles limparam, o próprio maitre o fez.","{'entities': [(136, 140, 'BEBIDA'), (10, 21, 'SERVIÇO'), (198, 204, 'SERVIÇO')]}"
+pela linha 11 fold 2
+"Ambiente agradável. Atendimento excelente. Os pratos não são baratos, mas valem cada centavo e são bem servidos. O meu prato preferido é a carne de sol do sertão, arroz de leite e carne desfiada. Delícia.","{'entities': [(0, 8, 'AMBIENTE'), (46, 52, 'COMIDA'), (119, 124, 'COMIDA'), (139, 161, 'COMIDA'), (163, 177, 'COMIDA'), (180, 194, 'COMIDA'), (20, 31, 'SERVIÇO'), (61, 68, 'PREÇO'), (80, 92, 'PREÇO')]}"
+
+Balanceei bebidas no teste
+
+Agora balacear bebidas no Treino
+
+
+pela linha 31 fold 4
+"Ótimo prato, chopp gelado, vinho branco muito bom...excelente atendimento! O Garçom Edvaldo nos atendeu muito bem!!!","{'entities': [(6, 11, 'COMIDA'), (13, 18, 'BEBIDA'), (27, 39, 'BEBIDA'), (62, 73, 'SERVIÇO'), (77, 91, 'SERVIÇO')]}"
+troca linha 225 fold 1
+"Prato generoso para 2 pessoas! Muito saboroso, porém possui coentro e não especificam no cardápio! Como não gosto, tive que separar.","{'entities': [(0, 5, 'COMIDA'), (60, 67, 'COMIDA'), (89, 97, 'COMIDA')]}"
+troca linha 247 fold 1
+"Fomos atendidos pelo Domingos Silva, muito atencioso, comida muito boa e ambiente agradável. RecomendoMais","{'entities': [(73, 81, 'AMBIENTE'), (54, 60, 'COMIDA'), (21, 35, 'SERVIÇO')]}"
+pela linha 84 fold 4
+"O Coco Bambu de Uberlândia segue o padrão da rede que, em geral, é bom. Porém, a unidade local sofre de um sério problema com o serviço. O tempo para trazerem uma simples água é inacreditável. Uma bebida mais elaborada, pior ainda. Já os pratos......Mais","{'entities': [(89, 94, 'GERAL'), (238, 244, 'COMIDA'), (171, 175, 'BEBIDA'), (197, 203, 'BEBIDA'), (128, 135, 'SERVIÇO'), (2, 26, 'LOCALIZAÇÃO')]}"
+troca linha 102 fold 1
+"O Eduardo foi bastante atencioso, ótimo atendimento! A comida é deliciosa e caprichada, vale a visita.","{'entities': [(55, 61, 'COMIDA'), (2, 9, 'SERVIÇO'), (40, 51, 'SERVIÇO')]}"
+pela linha 96 fold 3
+"Tradicionalmente o melhor camarão da cidade, com ambiente agradável com vista para o mar; boa disponibilidade de vinhos e cervejas especiais. Bom para almoço, jantar e confraternizações.","{'entities': [(49, 57, 'AMBIENTE'), (72, 77, 'AMBIENTE'), (26, 33, 'COMIDA'), (151, 157, 'COMIDA'), (159, 165, 'COMIDA'), (113, 119, 'BEBIDA'), (122, 140, 'BEBIDA')]}"
+troca linha 111 fold 5
+"Ambiente agradável. Atendimento excelente. Os pratos não são baratos, mas valem cada centavo e são bem servidos. O meu prato preferido é a carne de sol do sertão, arroz de leite e carne desfiada. Delícia.","{'entities': [(0, 8, 'AMBIENTE'), (46, 52, 'COMIDA'), (119, 124, 'COMIDA'), (139, 161, 'COMIDA'), (163, 177, 'COMIDA'), (180, 194, 'COMIDA'), (20, 31, 'SERVIÇO'), (61, 68, 'PREÇO'), (80, 92, 'PREÇO')]}"
+pela linha 334 fold 2
+"Excelente atendimento. Todos os profissionais, com quem tivemos contato,nos atenderam muito bem. Preocuparam-se com cada detalhe. Até o suco que meu filho derramou na roupa eles limparam, o próprio maitre o fez.","{'entities': [(136, 140, 'BEBIDA'), (10, 21, 'SERVIÇO'), (198, 204, 'SERVIÇO')]}"
